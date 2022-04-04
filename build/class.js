7abcfd6ef0d0441be3e2eaa8ee733a1dba710392
Richard Laurent <ronaldorichard27@gmail.com>
2022-04-04 22:50:17 +0700

    Ch. 17 (Super Constructor) --Typescript

--- build/class.js
+++ build/class.js
@@ -1,14 +1,42 @@
 "use strict";
-//TODO Class pada Typescript
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
+//TODO Class pada Typescript
 class User {
     //* name harus di definisikan terlebih dahulu pada Constructor agar Typescript tidak error
     constructor(name, age) {
         this.name = name;
         this.age = age;
+        this.getName = () => {
+            return this.name;
+        };
+    }
+    setName(value) {
+        this.name = value;
     }
 }
 exports.User = User;
-let user = new User("Richard Laurent", 22);
-console.log(user.name, user.age);
+//* Public = bisa diakses disemua class / luar class
+//* Protected = hanya bisa diakses dari class tersebut / class turunnya
+//* Private = hanya bisa diakses di class itu sendiri 
+// let user = new User("Richard Laurent", 22);
+// console.log(user.name, user.age);
+//TODO Inheritance Typescript
+class Admin extends User {
+    //* Super Constructor dengan syntax super()
+    constructor(phone, name, age) {
+        super(name, age);
+        this.read = true;
+        this.write = true;
+        this.phone = phone;
+    }
+    getRole() {
+        return {
+            read: this.read,
+            write: this.write
+        };
+    }
+    ;
+}
+let admin = new Admin("081210835622", "Richard Laurent", 22);
+console.log(admin.getName(), admin.getRole(), admin.phone);
--- src/class.ts
+++ src/class.ts
@@ -1,14 +1,53 @@
-
 //TODO Class pada Typescript
-
 export class User {
 
   //* name harus di definisikan terlebih dahulu pada Constructor agar Typescript tidak error
 
   constructor(public name: string, public age: number) {
   }
+
+  setName(value: string): void {
+    this.name = value;
+  }
+
+  getName = (): string => {
+    return this.name;
+  }
+
 }
 
-let user = new User("Richard Laurent", 22);
+//* Public = bisa diakses disemua class / luar class
+//* Protected = hanya bisa diakses dari class tersebut / class turunnya
+//* Private = hanya bisa diakses di class itu sendiri 
 
-console.log(user.name, user.age);
+// let user = new User("Richard Laurent", 22);
+// console.log(user.name, user.age);
+
+//TODO Inheritance Typescript
+
+class Admin extends User {
+  read: boolean = true;
+  write: boolean = true;
+  phone: string;
+
+  //* Super Constructor dengan syntax super()
+  constructor(phone: string, name: string, age: number) {
+    super(name, age);
+    this.phone = phone;
+  }
+
+  getRole(): { read: boolean, write: boolean } {
+    return {
+      read: this.read,
+      write: this.write 
+    };
+  };
+}
+
+let admin = new Admin("081210835622", "Richard Laurent", 22);
+
+console.log(
+  admin.getName(),
+  admin.getRole(),
+  admin.phone
+);
