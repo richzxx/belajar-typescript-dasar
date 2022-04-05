b872638dbb23134f137229b017995bcb7c536918
richardlaurents <richardlaurents04@gmail.com>
2022-04-05 22:59:33 +0700

    Add Acer Component and Update Product [--fix]

--- src/module/Product.ts
+++ src/module/Product.ts
@@ -1,12 +1,14 @@
 import Asus from "./Asus";
 import Macbook from "./Macbook";
+import Acer from "./Acer";
 
 let asus = new Asus("Zenbook", true, true);
 console.log(asus);
-asus.pressA();
-asus.pressB();
+// asus.pressA();
+// asus.pressB();
 
 let macbook = new Macbook(2017, false, false);
 console.log(macbook);
-asus.pressA();
-asus.pressB();
+
+let acer = new Acer("Nitro 5", false, false);
+console.log(acer);
